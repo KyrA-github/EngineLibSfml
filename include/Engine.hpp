38522afe84c3beb9be1871f47fbddfd38c2822aa
KyrA-github <korenovsostav@gmail.com>
2024-10-03 00:18:03 +0300

    +PIRAMID

--- include/Engine.hpp
+++ include/Engine.hpp
@@ -3,41 +3,173 @@
 
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <map>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <math.h>
 
-struct Polygon3 {
-    sf::VertexArray sfVerAry;
-    sf::Vector2f localOffset;
-    sf::Vector3f posVec3[3]; // Не изменяем, но мы поменяем использование
+struct Vertex {
+    float x, y, z;
 };
 
-struct Obj {
-    std::string name;
-    std::vector<Polygon3> polygons;
-    sf::Vector3f position;
-     // Метод для рисования объекта с учётом позиции камеры
-    void draw(sf::RenderWindow* pWindow, const sf::Vector3f& sfVec3fCamera, float fDistanceToProjection) {
-        for (auto& polygon : polygons) {
-            // Рисуем основной треугольник
-            for (int i = 0; i < 3; ++i) {
-                sf::Vector3f relativePos = polygon.posVec3[i] - sfVec3fCamera;
-                polygon.sfVerAry[i].position = sf::Vector2f((relativePos.x * fDistanceToProjection) / relativePos.z + pWindow->getSize().x / 2,
-                        (relativePos.y * fDistanceToProjection) / relativePos.z + pWindow->getSize().y / 2);
+struct Polygon {
+    int vertex1, vertex2, vertex3;
+};
+
+class Object3D {
+public:
+    std::vector<Vertex> vertices;
+    std::vector<Polygon> polygons;
+    float posX, posY, posZ;
+
+    Object3D() : posX(0), posY(0), posZ(0) {}
+
+    void setPosition(float x, float y, float z) {
+        posX = x;
+        posY = y;
+        posZ = z;
+    }
+
+    // Проекция 3D вершины на 2D плоскость
+    sf::Vector2f projectVertex(const Vertex& vertex, float scale) {
+        float projectedX = (vertex.x + posX) / (vertex.z + posZ) * scale;
+        float projectedY = (vertex.y + posY) / (vertex.z + posZ) * scale;
+        return sf::Vector2f(projectedX, projectedY);
+    }
+
+    // Функция для рисования объекта
+    void draw(sf::RenderWindow& window, float scale) {
+        for (const Polygon& poly : polygons) {
+            sf::ConvexShape triangle;
+            triangle.setPointCount(3);
+
+            // Проекция каждой вершины полигона
+            sf::Vector2f p1 = projectVertex(vertices[poly.vertex1], scale);
+            sf::Vector2f p2 = projectVertex(vertices[poly.vertex2], scale);
+            sf::Vector2f p3 = projectVertex(vertices[poly.vertex3], scale);
+
+            triangle.setPoint(0, p1);
+            triangle.setPoint(1, p2);
+            triangle.setPoint(2, p3);
+
+            // Цвет и отрисовка
+            triangle.setFillColor(sf::Color::Green);
+            window.draw(triangle);
+        }
+    }
+
+    bool loadFromFile(const std::string& filename) {
+        std::ifstream file(filename);
+        if (!file.is_open()) {
+            std::cerr << "Ошибка открытия файла: " << filename << std::endl;
+            return false;
+        }
+
+        std::string line;
+        while (std::getline(file, line)) {
+            std::istringstream iss(line);
+            char type;
+            iss >> type;
+
+            if (type == 'v') {
+                // Чтение вершины
+                float x, y, z;
+                iss >> x >> y >> z;
+                addVertex(x, y, z);
+            } else if (type == 'p') {
+                // Чтение полигона
+                int v1, v2, v3;
+                iss >> v1 >> v2 >> v3;
+                addPolygon(v1 - 1, v2 - 1, v3 - 1);  // Индексы вершин начинаются с 0
             }
-            pWindow->draw(polygon.sfVerAry);
+        }
 
-            // Теперь создаём обводку
-            sf::VertexArray outline(sf::LineStrip, 4);  // 4 точки: 3 вершины + 1 повторение для замыкания
+        return true;
+    }
+    void addVertex(float x, float y, float z) {
+        vertices.push_back({x, y, z});
+    }
 
-            for (int i = 0; i < 3; ++i) {
-                outline[i].position = polygon.sfVerAry[i].position;
-                outline[i].color = sf::Color::White;  // Цвет обводки, например, белый
+    void addPolygon(int v1, int v2, int v3) {
+        polygons.push_back({v1, v2, v3});
+    }
+    // Функция для рисования граней (ребер) объекта
+    void drawEdges(sf::RenderWindow& window, float scale) {
+        for (const Polygon& poly : polygons) {
+            sf::VertexArray lines(sf::LinesStrip, 4);
+
+            // Проекция каждой вершины полигона
+            sf::Vector2f p1 = projectVertex(vertices[poly.vertex1], scale);
+            sf::Vector2f p2 = projectVertex(vertices[poly.vertex2], scale);
+            sf::Vector2f p3 = projectVertex(vertices[poly.vertex3], scale);
+
+            // Соединение вершин линиями
+            lines[0].position = p1;
+            lines[1].position = p2;
+            lines[2].position = p3;
+            lines[3].position = p1;  // Замыкаем треугольник
+
+            // Устанавливаем цвет для линий
+            for (int i = 0; i < 4; i++) {
+                lines[i].color = sf::Color::White;
             }
-            outline[3].position = polygon.sfVerAry[0].position; // Замыкаем линию
 
-            // Рисуем обводку
-            pWindow->draw(outline);
+            window.draw(lines);
+        }
+    }void rotate(float angleX, float angleY, float angleZ) {
+        float radX = angleX * M_PI / 180;
+        float radY = angleY * M_PI / 180;
+        float radZ = angleZ * M_PI / 180;
+
+        // Матрицы вращения
+        float rotX[3][3] = {
+            {1, 0, 0},
+            {0, cos(radX), -sin(radX)},
+            {0, sin(radX), cos(radX)}
+        };
+
+        float rotY[3][3] = {
+            {cos(radY), 0, sin(radY)},
+            {0, 1, 0},
+            {-sin(radY), 0, cos(radY)}
+        };
+
+        float rotZ[3][3] = {
+            {cos(radZ), -sin(radZ), 0},
+            {sin(radZ), cos(radZ), 0},
+            {0, 0, 1}
+        };
+
+        // Применение вращения
+        for (Vertex& vertex : vertices) {
+            // Вращение вокруг оси X
+            float x1 = vertex.x;
+            float y1 = vertex.y * rotX[1][1] + vertex.z * rotX[1][2];
+            float z1 = vertex.y * rotX[2][1] + vertex.z * rotX[2][2];
+
+            vertex.x = x1;
+            vertex.y = y1;
+            vertex.z = z1;
+
+            // Вращение вокруг оси Y
+            float x2 = vertex.x * rotY[0][0] + vertex.z * rotY[0][2];
+            float y2 = vertex.y;
+            float z2 = -vertex.x * rotY[2][0] + vertex.z * rotY[2][2];
+
+            vertex.x = x2;
+            vertex.y = y2;
+            vertex.z = z2;
+
+            // Вращение вокруг оси Z
+            float x3 = vertex.x * rotZ[0][0] + vertex.y * rotZ[0][1];
+            float y3 = vertex.x * rotZ[1][0] + vertex.y * rotZ[1][1];
+            float z3 = vertex.z;
+
+            vertex.x = x3;
+            vertex.y = y3;
+            vertex.z = z3;
         }
     }
 };
@@ -46,29 +178,20 @@ class Engine {
 public:
     Engine(unsigned int uiWidth, unsigned int uiHeight);
     void render();
+
     ~Engine();
 
 private:
     sf::Vector3f sfVec3fCamera; // Позиция камеры
     sf::RenderWindow* pWindow;
-    float m_fDistanceToProjection;
-    float y = 1.0f; // Начальная координата Y для объектов (переменная переименована)
-    float z = 1.0f; // Начальная координата Z для объектов (переменная добавлена)
 
     unsigned int uiWidth;
     unsigned int uiHeight;
+    float fDistanceToProjection;
 
-    std::vector<Polygon3> vsTriangles;
-    std::vector<Obj> vsObjects;
-
-    void addTriangle(const sf::Vector3f& point1, const sf::Vector3f& point2, const sf::Vector3f& point3);
-    [[nodiscard]] sf::Vector2f projectTo2D(const sf::Vector3f& point) const;
-    void addObj(const sf::Vector3f &baseCenter, float baseSize, float height);
-    sf::Vector3f rotateX(const sf::Vector3f& point, float angle);
-    sf::Vector3f rotateY(const sf::Vector3f& point, float angle);
-    sf::Vector3f rotateZ(const sf::Vector3f& point, float angle);
-    void rotatePyramid(Obj& pyramid, float angleX, float angleY, float angleZ);
-    float getPolygonDepth(const Polygon3& polygon);
+    [[nodiscard]] sf::Vector2f _projectTo2D(const sf::Vector3f& point) const;
+    void _addObj(const sf::Vector3f &pos, const std::string &nameFile);
+    void _control();
 };
 
 #endif // ENGINE_HPP
--- src/Engine.cpp
+++ src/Engine.cpp
@@ -2,81 +2,34 @@
 #include <iostream>
 
 Engine::Engine(const unsigned int uiWidth, const unsigned int uiHeight)
-    : m_fDistanceToProjection(500), uiWidth(uiWidth), uiHeight(uiHeight) {
+    : uiWidth(uiWidth), uiHeight(uiHeight), fDistanceToProjection(500) {
     pWindow = new sf::RenderWindow(sf::VideoMode(uiWidth, uiHeight), "3D Engine");
     pWindow->setFramerateLimit(60);
 
     sfVec3fCamera = sf::Vector3f(0, 0, 0);
 
     std::cout << "Engine initialized with window size: " << uiWidth << "x" << uiHeight << std::endl;
-
-    addTriangle(sf::Vector3f(0, 0, 2), sf::Vector3f(1, 0, 2), sf::Vector3f(0.5f, 1, 2));
 }
 
-void Engine::addTriangle(const sf::Vector3f& point1, const sf::Vector3f& point2, const sf::Vector3f& point3) {
-    Polygon3 polygon;
-    sf::VertexArray triangle(sf::Triangles, 3);
-
-    triangle[0].position = projectTo2D(point1);
-    triangle[1].position = projectTo2D(point2);
-    triangle[2].position = projectTo2D(point3);
-
-    triangle[0].color = sf::Color::Red;
-    triangle[1].color = sf::Color::Green;
-    triangle[2].color = sf::Color::Blue;
-
-    polygon.sfVerAry = triangle;
-    polygon.posVec3[0] = point1;
-    polygon.posVec3[1] = point2;
-    polygon.posVec3[2] = point3;
-
-    vsTriangles.push_back(polygon);
-}
-void Engine::addObj(const sf::Vector3f& baseCenter, float baseSize, float height) {
-    Obj newObj;
-    newObj.name = "Pyramid"; // Установим имя объекта
-
-    // Вычисляем вершины пирамиды
-    sf::Vector3f topVertex(0, 0, height); // Вершина (пик) пирамиды
-
-    // Вершины основания (квадрат)
-    sf::Vector3f bottomVertices[4] = {
-        sf::Vector3f(baseCenter.x - baseSize / 2, baseCenter.y - baseSize / 2, baseCenter.z),
-        sf::Vector3f(baseCenter.x + baseSize / 2, baseCenter.y - baseSize / 2, baseCenter.z),
-        sf::Vector3f(baseCenter.x + baseSize / 2, baseCenter.y + baseSize / 2, baseCenter.z),
-        sf::Vector3f(baseCenter.x - baseSize / 2, baseCenter.y + baseSize / 2, baseCenter.z)
-    };
-
-    // Создаём 4 треугольника для боковых граней
-    for (int i = 0; i < 4; ++i) {
-        Polygon3 polygon;
-        sf::VertexArray triangle(sf::Triangles, 3);
-
-        // Вершины треугольника
-        triangle[0].position = projectTo2D(bottomVertices[i]);
-        triangle[1].position = projectTo2D(bottomVertices[(i + 1) % 4]);
-        triangle[2].position = projectTo2D(topVertex);
-
-        // Задаем цвета
-        triangle[0].color = sf::Color(128,128,128);   // Цвет первой вершины
-        triangle[1].color = sf::Color(128,128,128); // Цвет второй вершины
-        triangle[2].color = sf::Color(128,128,128);  // Цвет третьей вершины
-
-        polygon.sfVerAry = triangle;
-        polygon.posVec3[0] = bottomVertices[i];
-        polygon.posVec3[1] = bottomVertices[(i + 1) % 4];
-        polygon.posVec3[2] = topVertex;
-
-        newObj.polygons.push_back(polygon); // Добавляем полигон в объект
+Engine::~Engine() {
+    if (pWindow) {
+        delete pWindow;
+        pWindow = nullptr;
     }
+}
 
-    // Добавляем объект в вектор объектов (например, vsObjects)
-    vsObjects.push_back(newObj);
+void Engine::_addObj(const sf::Vector3f& pos, const std::string& nameFile) {
+
 }
 
 
 void Engine::render() {
-    addObj({0.134,0.134,0.134}, 1, 1); // Добавляем пирамиду
+    sfVec3fCamera.z = 1000;
+    Object3D object;
+    object.loadFromFile("untitled.obj");
+    object.setPosition(10, 10, 2);
+
+    float angleX = 0, angleY = 1, angleZ = 0;  // Углы вращения по осям
     while (pWindow->isOpen()) {
         sf::Event event{};
         while (pWindow->pollEvent(event)) {
@@ -91,89 +44,41 @@ void Engine::render() {
                 }
             }
         }
+        // Вращение объекта
 
-        // Обработка клавиш для перемещения камеры
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            sfVec3fCamera.y += 0.1f;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            sfVec3fCamera.y -= 0.1f;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            sfVec3fCamera.x -= 0.1f;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            sfVec3fCamera.x += 0.1f;
-        }
+        //angleX += 0.001f;
+        //angleY += 0.001f;
+        //angleZ += 0.001f;
 
+        object.rotate(angleX, angleY, angleZ);  // Вращаем объект
         pWindow->clear(sf::Color::Black);
 
-        // Рисуем все объекты
-        for (auto& obj : vsObjects) {
-            rotatePyramid(obj, 0, 1, 0); // Вращаем пирамиду в реальном времени
-            obj.draw(pWindow, sfVec3fCamera, m_fDistanceToProjection);
-        }
+        // Отрисовка объекта
+        object.draw(*pWindow, sfVec3fCamera.z);  // 100 - масштаб для проекции
 
         pWindow->display();
     }
 }
+void Engine::_control() {
+    // Обработка клавиш для перемещения камеры
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+        sfVec3fCamera.y += 0.1f;
 
-
-Engine::~Engine() {
-    if (pWindow) {
-        delete pWindow;
-        pWindow = nullptr;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+        sfVec3fCamera.y -= 0.1f;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+        sfVec3fCamera.x -= 0.1f;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        sfVec3fCamera.x += 0.1f;
     }
 }
 
-sf::Vector2f Engine::projectTo2D(const sf::Vector3f& point) const {
-    if (point.z <= 0) return sf::Vector2f(uiWidth / 2, uiHeight / 2); // Предотвращение деления на ноль или отрицательного z
-
-    return sf::Vector2f(
-        (point.x * m_fDistanceToProjection) / point.z + uiWidth / 2,
-        (point.y * m_fDistanceToProjection) / point.z + uiHeight / 2
-    );
+sf::Vector2f Engine::_projectTo2D(const sf::Vector3f& point) const {
+    return {
+        (point.x * fDistanceToProjection) / point.z,
+        (point.y * fDistanceToProjection) / point.z
+    };
 }
-
-sf::Vector3f Engine::rotateX(const sf::Vector3f& point, float angle) {
-    float rad = angle * (M_PI / 180);
-    return sf::Vector3f(
-        point.x,
-        point.y * cos(rad) - point.z * sin(rad),
-        point.y * sin(rad) + point.z * cos(rad)
-    );
-}
-
-sf::Vector3f Engine::rotateY(const sf::Vector3f& point, float angle) {
-    float rad = angle * (M_PI / 180);
-    return sf::Vector3f(
-        point.x * cos(rad) + point.z * sin(rad),
-        point.y,
-        -point.x * sin(rad) + point.z * cos(rad)
-    );
-}
-
-sf::Vector3f Engine::rotateZ(const sf::Vector3f& point, float angle) {
-    float rad = angle * (M_PI / 180);
-    return sf::Vector3f(
-        point.x * cos(rad) - point.y * sin(rad),
-        point.x * sin(rad) + point.y * cos(rad),
-        point.z
-    );
-}
-
-void Engine::rotatePyramid(Obj& pyramid, float angleX, float angleY, float angleZ) {
-    for (auto& polygon : pyramid.polygons) {
-        for (int i = 0; i < 3; ++i) {
-            // Вращаем по всем осям
-            polygon.posVec3[i] = rotateX(polygon.posVec3[i], angleX);
-            polygon.posVec3[i] = rotateY(polygon.posVec3[i], angleY);
-            polygon.posVec3[i] = rotateZ(polygon.posVec3[i], angleZ);
-
-            // Проецируем 3D координаты в 2D
-            sf::Vector3f relativePos = polygon.posVec3[i] - sfVec3fCamera;
-            polygon.sfVerAry[i].position = projectTo2D(relativePos);
-        }
-    }
-}
-
